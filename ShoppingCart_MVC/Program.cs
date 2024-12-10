34d835a94f5ad5da8b25625402bfe3dfc088c6be
Amoriss <amoriss@protonmail.com>
2024-12-09 23:53:30 -0600

    ApplicationDbContext is configured with MySQL in Program.cs and sample data is seeded into database

--- ShoppingCart_MVC/Program.cs
+++ ShoppingCart_MVC/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart_MVC.Data;
+using ShoppingCart_MVC.Models;
+using System.Runtime.CompilerServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+   options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+   new MySqlServerVersion(new Version(8,0,33))));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -24,4 +33,25 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    SeedDatabase(context);
+}
+
 app.Run();
+
+static void SeedDatabase (ApplicationDbContext context)
+{
+    if(!context.Items.Any())
+    {
+        context.Items.AddRange(
+            new Item { Name = "Product1", Price = 20.00},
+            new Item { Name = "Product2", Price = 15.00},
+            new Item { Name = "Product3", Price = 12.99},
+            new Item { Name = "Product4", Price = 3.50}
+            );
+
+    }
+
+}
