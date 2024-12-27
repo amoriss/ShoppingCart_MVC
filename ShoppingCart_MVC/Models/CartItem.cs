62f04037aa675ccbb6c06254051c1cdc98a0f4b1
Amoriss <amoriss@protonmail.com>
2024-12-26 23:47:31 -0600

    image url now displaying properly with size of the image adjusted

--- ShoppingCart_MVC/Models/CartItem.cs
+++ ShoppingCart_MVC/Models/CartItem.cs
@@ -6,5 +6,6 @@ public class CartItem
     public string Name { get; set; }
     public double Price { get; set; }
     public int Quantity { get; set; }
+    public string ImageUrl { get; set; }
 
 }
